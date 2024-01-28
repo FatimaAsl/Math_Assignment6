1b5b0f437260dec54efebfe46f6d6dcdfd9bfa42
Fatima <Fatemeh.aghapourasl@gmail.com>
2024-01-28 14:06:23 -0500

    Update trainable_layers.py

--- trainable_layers.py
+++ trainable_layers.py
@@ -8,20 +8,34 @@ def define_dense_model_with_hidden_layers(input_length,
                                           hidden_layers_sizes=[50, 20],
                                           output_function='softmax',
                                           output_length=10):
+                                         
+    """Define a dense model with a hidden layer with the following parameters:
+    input_length: the number of inputs for the first layer
+    activation_func_array: the activation function for the hidden layers
+    hidden_layer_sizes: the number of neurons in each of the hidden layer
+    output_function: the activation function for the output layer
+    output_length: the number of outputs (number of neurons in the output layer)"""
 
     model = keras.Sequential()
     # Create the input layer
-    model.add(layers.input(shape=(input_length)))
+    model.add(layers.Dense(units=hidden_layers_sizes[0], activation=activation_func_array[0], input_shape=(input_length,)))
     # Create the hidden layers
-    for i, in enumerate(hidden_layers_sizes):
-        activation_func =activation_func_array[i] if i< len(activation_func_array) else activation_func[-1]
-        model.add(layers.Dense(size, activation=activation_func))
+    for i in range(1, len(hidden_layers_sizes)):
+            model.add(layers.Dense(units=hidden_layers_sizes[i], activation=activation_func_array[i]))
     # Create the output layer
     model.add(layers.Dense(output_length, activation=output_function))
     return model
 
 
 def set_layers_to_trainable(model, trainable_layer_numbers):
+    """Set the layers of the model to trainable or not trainable.
+    model: the model
+    trainable_layer_numbers: the numbers of the layers to be set to trainable. set the other layers to not trainable
+    """
+    # Set the layers to trainable or not trainable
     for i, layer in enumerate(model.layers):
-        layer.trainable= i in trainable_layer_numbers
+        if i in trainable_layer_numbers:
+            layer.trainable = True
+        else:
+            layer.trainable = False
     return model
